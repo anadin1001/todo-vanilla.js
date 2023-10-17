ebb93c5a93e7d1545eed005f0fad182f3d256bba
AnaDin101 <anadin.amd@gmail.com>
2023-10-17 19:37:54 +0300

    draft login

--- Login/login.js
+++ Login/login.js
@@ -1,4 +1,28 @@
-export function login(e){
-    console.log("Vrei sa te autentifici");
-    e.preventDefault(); // face ca elementul sa ramana in consola
+import { users } from "../Database/users.js";
+
+export function login(e) {
+  e.preventDefault(); // face ca elementul sa ramana in consola
+  const loginEmail = document.getElementById("loginEmail");
+  const logindPassword = document.getElementById("loginPassword");
+  console.log(
+    `Vrei sa te autentifici cu ${loginEmail.value} si ${logindPassword.value} `
+  );
+
+  const user = users.filter(function (user) {
+    return user.email === loginEmail.value;
+  });
+  if (user[0]) {
+    console.log("utilizator gasit: ", user[0]);
+    if (user[0].password === logindPassword.value) {
+      localStorage.setItem("isLoggedIn", true);
+      localStorage.setItem("username", user[0].name);
+      document.getElementById("login").setAttribute("hidden", true);
+      document.getElementById("register").setAttribute("hidden", true);
+      document.getElementById("user").removeAttribute("hidden");
+      document.getElementById("greetings").textContent =
+        "Salut " + localStorage.getItem('username');
+    }
+  } else {
+    console.log("Utilizator negasit!");
+  }
 }
